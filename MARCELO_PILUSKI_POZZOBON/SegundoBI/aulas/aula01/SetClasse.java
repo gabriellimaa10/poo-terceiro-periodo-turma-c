fb93791fb0f6946ddf2503135dfdf10b96786ca7
frvnceschini <gpfranceschini@minha.fag.edu.br>
2024-05-09 21:49:38 -0300

    aula 09/05 21:49

--- MARCELO_PILUSKI_POZZOBON/SegundoBI/aulas/aula01/SetClasse.java
+++ MARCELO_PILUSKI_POZZOBON/SegundoBI/aulas/aula01/SetClasse.java
@@ -1,9 +1,10 @@
-package SegundoBI.aulas.Aula01.java;
+package SegundoBI.aulas.aula01;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Aula01 {
+public class SetClasse {
       public static void main(String[] args) {
             Set<String> nomeNaoRepetidos = new HashSet<>();
 
@@ -18,6 +19,6 @@ public class Aula01 {
 
             nomeNaoRepetidos.add("João");
 
-            nomeNaoRepetidos.forEach(item => System.out.println(item));
+            nomeNaoRepetidos.forEach(item -> System.out.println(item)); //Arrow function é com - ao invés de =
       }
 }
